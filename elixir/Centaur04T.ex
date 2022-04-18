185e5fdad7c33c7fb2cd4ffb419468477a3730fb
Mich404Teapod <101505439+Mich404Teapod@users.noreply.github.com>
2022-04-18 17:30:55 +0100

    Create Centaur04T.ex

---
+++ elixir/Centaur04T.ex
@@ -0,0 +1,11 @@
+defmodule Centaur04T.Scanner do
+  @moduledoc """
+  Specification for scanner wrappers
+  """
+
+  @doc """
+  Perform file scan
+  """
+  @callback scan(path :: Path.t()) ::
+              :ok | {:error, atom()} | {:error, String.t()}
+end
